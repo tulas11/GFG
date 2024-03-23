33e89bb2276dc7e6d401e7fb06415d67968f31b8
Yash Tulaskar <78523648+tulas11@users.noreply.github.com>
2024-03-23 23:30:11 +0530

    Update second-largest.py

--- Easy/Second Largest/second-largest.py
+++ Easy/Second Largest/second-largest.py
@@ -1,12 +1,36 @@
-#User function Template for python3
 class Solution:
-	def print2largest(self,arr, n):
-		# code here
-		arry=sorted(set(arr))
-		if len(arry)==1:
-		    return -1
-		else:
-		    return arry[-2]
+    def print2largest(self, arr, n):
+        first = second = float('-inf')
+
+        # There should be at least two elements
+        if n < 2:
+            return -1
+
+        for i in range(n):
+            # If current element is greater
+            # than first then update both
+            # first and second
+            if arr[i] > first:
+                second = first
+                first = arr[i]
+
+            # If arr[i] is in between first
+            # and second then update second
+            elif arr[i] > second and arr[i] != first:
+                second = arr[i]
+
+        if second == float('-inf'):
+            return -1
+        else:
+            return second
+# class Solution:
+# 	def print2largest(self,arr, n):
+# 		# code here
+# 		arry=sorted(set(arr))
+# 		if len(arry)==1:
+# 		    return -1
+# 		else:
+# 		    return arry[-2]
 
 #{ 
  # Driver Code Starts
